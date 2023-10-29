26d0fa1382f408e92b167c46890be47c89fc011f
Alexander Kachan <akach.dev@mail.ru>
2023-10-29 18:01:06 +0300

    lesson HW10 done

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {AppStoreType} from './bll/store'
-import {loadingAC} from './bll/loadingReducer'
+import {IsLoadingType, loadingAC} from './bll/loadingReducer'
 import SuperButton from '../hw04/common/c2-SuperButton/SuperButton'
 import s2 from '../../s1-main/App.module.css'
 import {Loader} from './Loader'
@@ -14,35 +14,37 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
+  // useSelector, useDispatch // пишет студент
+  const isLoading = useSelector<AppStoreType, boolean>(state => state.loading.isLoading)
+  const dispatch = useDispatch()
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+  const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
+    dispatch(loadingAC(true))
+    const timer = setTimeout(() => {
+      dispatch(loadingAC(false))
+    }, 1500)
+  }
 
-        // setTimeout
-    }
+  return (
+     <div id={'hw10'}>
+       <div className={s2.hwTitle}>Homework #10</div>
 
-    return (
-        <div id={'hw10'}>
-            <div className={s2.hwTitle}>Homework #10</div>
-
-            <div className={s2.hw}>
-                {isLoading ? (
-                    <div id={'hw10-loading'}>
-                        <Loader/>
-                    </div>
-                ) : (
-                    <SuperButton
-                        id={'hw10-button-start-loading'}
-                        onClick={setLoading}
-                    >
-                        Set loading...
-                    </SuperButton>
-                )}
+       <div className={s2.hw}>
+         {isLoading ? (
+            <div id={'hw10-loading'}>
+              <Loader/>
             </div>
-        </div>
-    )
+         ) : (
+            <SuperButton
+               id={'hw10-button-start-loading'}
+               onClick={setLoading}
+            >
+              Set loading...
+            </SuperButton>
+         )}
+       </div>
+     </div>
+  )
 }
 
 export default HW10
--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,16 @@
 import s from './Loader.module.css'
+import {Box, CircularProgress} from "@mui/joy";
 
-export const Loader = () => <div className={s.loader}/>
+
+export const Loader = () => <div className={s.loader}>
+
+  <CircularProgress
+     sx={{
+       "--CircularProgress-size": "90px",
+       "--CircularProgress-trackThickness": "0px",
+       "--CircularProgress-progressThickness": "9px"
+     }}
+  />
+
+
+</div>
